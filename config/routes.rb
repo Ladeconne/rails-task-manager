32b573982cf4790da4f9399f90e032b233e842ba
Yves Hurdequint <hurdequinty@hotmail.com>
2021-07-20 13:12:37 +0800

    new and create created

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # show all tasks
   get 'tasks', to: 'tasks#index', as: :tasks
+  # Go to form for new task
+  get 'tasks/new', to: 'tasks#new', as: :new_task
+  # show a specific task
   get 'tasks/:id', to: 'tasks#show', as: :task
+  # Create the tasks in the database on submit of new task form
+  post 'tasks', to: 'tasks#create'
 end
